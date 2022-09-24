48f967a542e4c7016b30f94088f389b67d138ed3
junaidjaan1388 <112433148+junaidjaan1388@users.noreply.github.com>
2022-09-24 23:57:04 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,344 +1,113 @@
-import uuid
-import math
-import asyncio
-import logging
-import threading
-import os, unittest, time, datetime
-import urllib.request, urllib.error, urllib.parse
+# © TamilBots 2021-22
+
 from pyrogram import Client, filters
-from yt_dlp import YoutubeDL
-from asyncio import get_running_loop
-from functools import partial
-from hachoir.metadata import extractMetadata
-from hachoir.parser import createParser
-import pyrogram
-from pyrogram import Client, filters
-from pyrogram.errors import MessageNotModified
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.errors import UserNotParticipant, UserBannedInChannel
-import shutil
-
-
-is_downloading = False
-
-logging.basicConfig(
-    level=logging.WARNING,
-    format='%(name)s - [%(levelname)s] - %(message)s'
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
 )
-LOGGER = logging.getLogger(__name__)
+import youtube_dl
+from youtube_search import YoutubeSearch
+import requests
 
-# --- PROGRESS DEF --- #
-async def progress_bar(current, total, text, message, start):
+import os
+from config import Config
 
-    now = time.time()
-    diff = now-start
-    if round(diff % 10) == 0 or current == total:
-        percentage = current*100/total
-        speed = current/diff
-        elapsed_time = round(diff)*1000
-        eta = round((total-current)/speed)*1000
-        ett = eta + elapsed_time
+bot = Client(
+    'SongPlayRoBot',
+    bot_token = Config.BOT_TOKEN,
+    api_id = Config.API_ID,
+    api_hash = Config.API_HASH
+)
 
-        elapsed_time = TimeFormatter(elapsed_time)
-        ett = TimeFormatter(ett)
+## Extra Fns -------------------------------
 
-        progress = "[{0}{1}] \n\n🔹Progress: {2}%\n".format(
-            ''.join(["◼️" for i in range(math.floor(percentage / 5))]),
-            ''.join(["◻️" for i in range(20 - math.floor(percentage / 5))]),
-            round(percentage, 2))
+# Convert hh:mm:ss to seconds
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
 
-        tmp = progress + "{0} of {1}\n\n️🔹Speed: {2}/s\n\n🔹ETA: {3}\n".format(
-            humanbytes(current),
-            humanbytes(total),
-            humanbytes(speed),
-            # elapsed_time if elapsed_time != '' else "0 s",
-            ett if ett != '' else "0 s"
+
+## Commands --------------------------------
+@bot.on_message(filters.command(['start']))
+def start(client, message):
+    TamilBots = f'👋 𝗛𝗲𝗹𝗹𝗼 @{message.from_user.username}\n\n𝗜 𝗔𝗺 🎸𝐒𝐨𝐧𝐠 𝐏𝐥𝐚𝐲 𝐁𝐨𝐭[🎶](https://telegra.ph/file/6cb884fe1cb943ec12df1.mp4)\n\n𝗦𝗲𝗻𝗱 𝗧𝗵𝗲 𝗡𝗮𝗺𝗲 𝗢𝗳 𝗧𝗵𝗲 𝗦𝗼𝗻𝗴 𝗬𝗼𝘂 𝗪𝗮𝗻𝘁... 😍🥰🤗\n\n𝗧𝘆𝗽𝗲 /s 𝗦𝗼𝗻𝗴 𝗡𝗮𝗺𝗲\n\n𝐄𝐠. `/s Faded`'
+    message.reply_text(
+        text=TamilBots, 
+        quote=False,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton('𝐒𝐔𝐏𝐏𝐎𝐑𝐓 👬', url='https://t.me/TamilSupport'),
+                    InlineKeyboardButton('𝐀𝐃𝐃 𝐌𝐄 🤗', url='https://t.me/SongProBot?startgroup=true')
+                ]
+            ]
         )
+    )
 
-        try :
-            await message.edit(
-                text = '{}.\n{}'.format(text, tmp)
-            )
-        except:
-            pass
-
-def humanbytes(size):
-    # https://stackoverflow.com/a/49361727/4723940
-    # 2**10 = 1024
-    if not size:
-        return ""
-    power = 2**10
-    n = 0
-    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
-    while size > power:
-        size /= power
-        n += 1
-    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
-
-
-def TimeFormatter(milliseconds: int) -> str:
-    seconds, milliseconds = divmod(int(milliseconds), 1000)
-    minutes, seconds = divmod(seconds, 60)
-    hours, minutes = divmod(minutes, 60)
-    days, hours = divmod(hours, 24)
-    tmp = ((str(days) + "d, ") if days else "") + \
-        ((str(hours) + "h, ") if hours else "") + \
-        ((str(minutes) + "m, ") if minutes else "") + \
-        ((str(seconds) + "s, ") if seconds else "") + \
-        ((str(milliseconds) + "ms, ") if milliseconds else "")
-    return tmp[:-2]
-
-# --- YTDL DOWNLOADER --- #
-def ytdl_dowload(result, opts):
-    global is_downloading
+@bot.on_message(filters.command(['s']))
+def a(client, message):
+    query = ''
+    for i in message.command[1:]:
+        query += ' ' + str(i)
+    print(query)
+    m = message.reply('🔎 𝐒𝐞𝐚𝐫𝐜𝐡𝐢𝐧𝐠 𝐭𝐡𝐞 𝐬𝐨𝐧𝐠...')
+    ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
-        with YoutubeDL(opts) as ytdl:
-            ytdl.cache.remove()
-            ytdl_data = ytdl.extract_info(result)
+        results = []
+        count = 0
+        while len(results) == 0 and count < 6:
+            if count>0:
+                time.sleep(1)
+            results = YoutubeSearch(query, max_results=1).to_dict()
+            count += 1
+        # results = YoutubeSearch(query, max_results=1).to_dict()
+        try:
+            link = f"https://youtube.com{results[0]['url_suffix']}"
+            # print(results)
+            title = results[0]["title"]
+            thumbnail = results[0]["thumbnails"][0]
+            duration = results[0]["duration"]
+
+            ## UNCOMMENT THIS IF YOU WANT A LIMIT ON DURATION. CHANGE 1800 TO YOUR OWN PREFFERED DURATION AND EDIT THE MESSAGE (30 minutes cap) LIMIT IN SECONDS
+            # if time_to_seconds(duration) >= 1800:  # duration limit
+            #     m.edit("Exceeded 30mins cap")
+            #     return
+
+            views = results[0]["views"]
+            thumb_name = f'thumb{message.message_id}.jpg'
+            thumb = requests.get(thumbnail, allow_redirects=True)
+            open(thumb_name, 'wb').write(thumb.content)
+
+        except Exception as e:
+            print(e)
+            m.edit('𝐅𝐨𝐮𝐧𝐝 𝐍𝐨𝐭𝐡𝐢𝐧𝐠. 𝐓𝐫𝐲 𝐂𝐡𝐚𝐧𝐠𝐢𝐧𝐠 𝐓𝐡𝐞 𝐒𝐩𝐞𝐥𝐥𝐢𝐧𝐠 𝐀 𝐋𝐢𝐭𝐭𝐥𝐞 😕')
+            return
     except Exception as e:
-        is_downloading = False
+        m.edit(
+            "✖️ 𝐅𝐨𝐮𝐧𝐝 𝐍𝐨𝐭𝐡𝐢𝐧𝐠. 𝐒𝐨𝐫𝐫𝐲.\n\n𝐓𝐫𝐲 𝐀𝐧𝐨𝐭𝐡𝐞𝐫 𝐊𝐞𝐲𝐰𝐨𝐫𝐤 𝐎𝐫 𝐌𝐚𝐲𝐛𝐞 𝐒𝐩𝐞𝐥𝐥 𝐈𝐭 𝐏𝐫𝐨𝐩𝐞𝐫𝐥𝐲.\n\nEg.`/s Faded`"
+        )
+        print(str(e))
+        return
+    m.edit("🔎 𝐅𝐢𝐧𝐝𝐢𝐧𝐠 𝐀 𝐒𝐨𝐧𝐠 🎶 𝐏𝐥𝐞𝐚𝐬𝐞 𝐖𝐚𝐢𝐭 ⏳️ 𝐅𝐨𝐫 𝐅𝐞𝐰 𝐒𝐞𝐜𝐨𝐧𝐝𝐬 [🚀](https://telegra.ph/file/67f41ae52a85dfc0551ae.mp4)")
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        rep = f'🎧 𝐓𝐢𝐭𝐥𝐞 : [{title[:35]}]({link})\n⏳ 𝐃𝐮𝐫𝐚𝐭𝐢𝐨𝐧 : `{duration}`\n🎬 𝐒𝐨𝐮𝐫𝐜𝐞 : [Youtube](https://youtu.be/3pN0W4KzzNY)\n👁‍🗨 𝐕𝐢𝐞𝐰𝐬 : `{views}`\n\n💌 𝐁𝐲 : @SongPlayRoBot'
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr)-1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
+            secmul *= 60
+        message.reply_audio(audio_file, caption=rep, parse_mode='md',quote=False, title=title, duration=dur, thumb=thumb_name)
+        m.delete()
+    except Exception as e:
+        m.edit('❌ 𝐄𝐫𝐫𝐨𝐫\n\n Report This Erorr To Fix @TamilSupport ❤️')
+        print(e)
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
         print(e)
 
-
-UPDTE_CHNL = os.environ.get("UPDTE_CHNL")
-LOG_CHNL = os.environ.get("LOG_CHNL")
-
-@Client.on_message(filters.regex(pattern=".*http.* (.*)"))
-async def uloader(client, message):
-    global is_downloading
-
-    if UPDTE_CHNL:
-        if not (await pyro_fsub(client, message, UPDTE_CHNL) == True):
-            return
-
-    if is_downloading:
-        return await message.reply_text(
-            "`Another download is in progress, try again after sometime.`",
-            quote=True
-        )
-
-    url = message.text.split(None, 1)[0]
-    typee = message.text.split(None, 1)[1]
-    if url.__contains__("/channel/") or url.__contains__("/c/"):
-        msg = await client.send_message(message.chat.id, '`Processing...`', reply_to_message_id=message.message_id)
-    else:
-        return await client.send_message(message.chat.id, '`I think this is invalid link...`', reply_to_message_id=message.message_id)
-
-    out_folder = f"downloads/{uuid.uuid4()}/"
-    if not os.path.isdir(out_folder):
-        os.makedirs(out_folder)
-
-    if (os.environ.get("USE_HEROKU") == "True") and (typee == "audio"):
-        opts = {
-            'format':'bestaudio[ext=m4a]',
-            'cachedir':False,
-            'addmetadata':True,
-            'geo_bypass':True,
-            'nocheckcertificate':True,
-            'outtmpl':out_folder + '%(title)s.%(ext)s',
-            'quiet':False,
-            'logtostderr':False
-        }
-        video = False
-        song = True
-    elif (os.environ.get("USE_HEROKU") == "False") and (typee == "audio"):
-        opts = {
-            'format':'bestaudio',
-            'cachedir':False,
-            'addmetadata':True,
-            'key':'FFmpegMetadata',
-            'prefer_ffmpeg':True,
-            'geo_bypass':True,
-            'nocheckcertificate':True,
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '320',
-            }],
-            'outtmpl':out_folder + '%(title)s.%(ext)s',
-            'quiet':False,
-            'logtostderr':False
-        }
-        video = False
-        song = True
-
-    if (os.environ.get("USE_HEROKU") == "False") and (typee == "video"):
-        opts = {
-            'format':'best',
-            'cachedir':False,
-            'addmetadata':True,
-            'xattrs':True,
-            'key':'FFmpegMetadata',
-            'prefer_ffmpeg':True,
-            'geo_bypass':True,
-            'nocheckcertificate':True,
-            'postprocessors': [{
-                'key': 'FFmpegVideoConvertor',
-                'preferedformat': 'mp4'},],
-            'outtmpl':out_folder + '%(title)s.%(ext)s',
-            'logtostderr':False,
-            'quiet':False
-        }
-        song = False
-        video = True
-    elif (os.environ.get("USE_HEROKU") == "True") and (typee == "video"):
-        opts = {
-            'format':'best',
-            'cachedir':False,
-            'addmetadata':True,
-            'xattrs':True,
-            'geo_bypass':True,
-            'nocheckcertificate':True,
-            'videoformat':'mp4',
-            'outtmpl':out_folder + '%(title)s.%(ext)s',
-            'logtostderr':False,
-            'quiet':False
-        }
-        song = False
-        video = True
-    is_downloading = True
-
-    if LOG_CHNL:
-        await client.send_message(LOG_CHNL, f"Name: {message.from_user.mention}\nURL: {url} {typee}")
-
-    try:
-        await msg.edit("`Downloading...`")
-        loop = get_running_loop()
-        await loop.run_in_executor(None, partial(ytdl_dowload, url, opts))
-        filename = sorted(get_lst_of_files(out_folder, []))
-    except Exception as e:
-        is_downloading = False
-        return await msg.edit("Error: "+e)
-
-    c_time = time.time()
-    try:
-        await msg.edit("`Uploading...`")
-    except MessageNotModified:
-        pass
-
-    if song:
-        for single_file in filename:
-            if os.path.exists(single_file):
-                if single_file.endswith((".mp4", ".mp3", ".flac", ".m4a", ".webm")):
-                    try:
-                        audio_name = os.path.basename(single_file)
-                        audioname = audio_name.replace('.'+audio_name.rsplit(".", 1)[1], '')
-                        tnow = time.time()
-                        fduration, fwidth, fheight = get_metadata(single_file)
-                        await message.reply_chat_action("upload_audio")
-                        await client.send_audio(
-                            message.chat.id,
-                            single_file,
-                            caption=f"`{audioname}`",
-                            duration=fduration,
-                            progress=progress_bar,
-                            progress_args=(
-                                "Uploading..", msg, c_time
-                            )
-                        )
-                    except Exception as e:
-                        await msg.edit("{} caused `{}`".format(single_file, str(e)))
-                        continue
-                    await message.reply_chat_action("cancel")
-                    os.remove(single_file)
-        LOGGER.info(f"Clearing {out_folder}")
-        shutil.rmtree(out_folder)
-        await del_old_msg_send_msg(msg, client, message)
-        is_downloading = False
-
-    if video:
-        for single_file in filename:
-            if os.path.exists(single_file):
-                if single_file.endswith((".mp4", ".m4a", ".mp3", ".flac", ".webm")):
-                    try:
-                        video_name = os.path.basename(single_file)
-                        videoname = video_name.replace('.'+video_name.rsplit(".", 1)[1], '')
-                        tnow = time.time()
-                        fduration, fwidth, fheight = get_metadata(single_file)
-                        await message.reply_chat_action("upload_video")
-                        await client.send_video(
-                            message.chat.id,
-                            single_file,
-                            caption=f"`{videoname}`",
-                            supports_streaming=True,
-                            duration=fduration,
-                            width=fwidth,
-                            height=fheight,
-                            progress=progress_bar,
-                            progress_args=(
-                                "Uploading..", msg, c_time
-                            )
-                        )
-                    except Exception as e:
-                        await msg.edit("{} caused `{}`".format(single_file, str(e)))
-                        continue
-                    await message.reply_chat_action("cancel")
-                    os.remove(single_file)
-        LOGGER.info(f"Clearing {out_folder}")
-        shutil.rmtree(out_folder)
-        await del_old_msg_send_msg(msg, client, message)
-        is_downloading = False
-
-
-def get_lst_of_files(input_directory, output_lst):
-    filesinfolder = os.listdir(input_directory)
-    for file_name in filesinfolder:
-        current_file_name = os.path.join(input_directory, file_name)
-        if os.path.isdir(current_file_name):
-            return get_lst_of_files(current_file_name, output_lst)
-        output_lst.append(current_file_name)
-    return output_lst
-
-async def del_old_msg_send_msg(msg, client, message):
-    await msg.delete()
-    await client.send_message(message.chat.id, "`Upload Success!`")
-
-def get_metadata(file):
-    fwidth = None
-    fheight = None
-    fduration = None
-    metadata = extractMetadata(createParser(file))
-    if metadata is not None:
-        if metadata.has("duration"):
-            fduration = metadata.get('duration').seconds
-        if metadata.has("width"):
-            fwidth = metadata.get("width")
-        if metadata.has("height"):
-            fheight = metadata.get("height")
-    return fduration, fwidth, fheight
-
-async def pyro_fsub(c, message, fsub):
-    try:
-        user = await c.get_chat_member(fsub, message.chat.id)
-        if user.status == "kicked":
-            await c.send_message(
-                chat_id=message.chat.id,
-                text="Sorry, You are Banned to use me.",
-                parse_mode="markdown",
-                disable_web_page_preview=True
-            )
-            return False
-        return True
-    except UserNotParticipant:
-        await c.send_message(
-            chat_id=message.chat.id,
-            text="**Please Join My Updates Channel to Use Me!**",
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton("Join Now", url=f"https://t.me/{UPDTE_CHNL}")
-                    ]
-                ]
-            )
-        )
-        return False
-    except Exception as kk:
-        print(kk)
-        await c.send_message(
-            chat_id=message.chat.id,
-            text="Something went Wrong.",
-            parse_mode="markdown",
-            disable_web_page_preview=True)
-        return False
-
-print("> Bot Started ")
+bot.run()
